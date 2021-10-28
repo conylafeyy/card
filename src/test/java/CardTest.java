d2040eca6c83f181e384a867e4d4c4db22f76498
Екатерина <laufeyconyy@gmail.com>
2021-10-28 13:40:34 +0300

    new driver2.0

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -22,7 +22,7 @@ public class CardTest {
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
